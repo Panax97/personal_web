488bccaf96c5792a068b93b04f464bfc21354dac
Panax97 <tcpanameno@gmail.com>
2026-01-07 13:01:00 -0500

    dws
    dsdsds

--- clients/victor_cleaning_solutions/website/portfolio.php
+++ clients/victor_cleaning_solutions/website/portfolio.php
@@ -39,9 +39,8 @@
 
     <div class="row">
       <?php
-        $galleryDir = rtrim($_SERVER['DOCUMENT_ROOT'], '/') 
-            . '/clients/victor_cleaning_solutions/webiste/victor_cleaning_solutions/images/fotos_victor/portfolio';
-        $galleryUrl = '/images/fotos_victor/portfolio';
+        $galleryDir = __DIR__ . '/images/fotos_victor/portfolio';
+        $galleryUrl = 'images/fotos_victor/portfolio';
         $allowedExt = ['jpg','jpeg','png','webp'];
 
         $files = [];
