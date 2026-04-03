40b6ac526780704bc1f9e4daf45d842f5868fbf2
Panax97 <tcpanameno@gmail.com>
2026-04-03 10:40:31 -0400

    df
    df

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - FlexStart Bootstrap Template</title>
+  <title>PIA | Data Analytics, Automation & Business Solutions</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/pia_icon.ico" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Fonts -->
